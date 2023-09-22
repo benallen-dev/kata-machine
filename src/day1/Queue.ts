5039d1c447aea402490f7ce3a884fcbdd6003637
Ben Allen <ben@benallen.dev>
2023-09-23 00:48:07 +0200

    Implemented a queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,51 @@
+type Node<T> = {
+	value: T,
+	next?: Node<T>,
+}
+
 export default class Queue<T> {
-    public length: number;
+	public length: number;
+	private head?: Node<T>;
+	private tail?: Node<T>;
 
-    
+	constructor() {
+		this.head = this.tail = undefined;
+		this.length = 0;
+	}
 
-    constructor() {
-    }
+	enqueue(item: T): void {
+		this.length++;
 
-    enqueue(item: T): void {
+		const node = { value: item } as Node<T>;
 
-}
-    deque(): T | undefined {
+		if (!this.tail) {
+			this.tail = this.head = node;
+			return;
+		}
 
-}
-    peek(): T | undefined {
+		this.tail.next = node;
+		this.tail = node;
+	}
 
-}
+	deque(): T | undefined {
+		if (!this.head) {
+			return undefined;
+		}
+
+		this.length--;
+
+		const head = this.head;
+		this.head = head.next;
+
+		head.next = undefined;
+		if (this.length === 0) {
+			this.tail = undefined;
+		}
+
+		return head.value;
+	}
+
+	peek(): T | undefined {
+		return this.head?.value;
+	}
 }
