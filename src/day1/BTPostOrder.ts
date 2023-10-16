69a76098bca513854a934911b182e1686c233a36
Ben Allen <ben@benallen.dev>
2023-10-16 13:02:46 +0200

    Depth-first tree traversal

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,21 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk (curr: BinaryNode<number> | null, path: number[]): number[] {
+	// Base case
+	if (!curr) {
+		return path
+	}
 
+	// Pre
+	
+	// Recurse
+	walk(curr.left, path);
+	walk(curr.right, path);
+
+	// Post
+	path.push(curr.value);	
+	return path;
 }
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+	return walk(head, []);
+}
+
--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,20 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk (curr: BinaryNode<number> | null, path: number[]): number[] {
+	// Base case
+	if (!curr) {
+		return path
+	}
 
+	// Pre
+	path.push(curr.value);	
+	
+	// Recurse
+	walk(curr.left, path);
+	walk(curr.right, path);
+
+	// Post
+	return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+	return walk(head, []);
 }
