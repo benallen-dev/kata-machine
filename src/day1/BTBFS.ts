e1b19beb2d0f0d7a1bee74e7e1b77a704ccff763
Ben Allen <ben@benallen.dev>
2023-10-30 18:40:58 +0100

    Implement BTBFS

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,24 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
 
+	const q = [head];
+
+	while(q.length) {
+		const node = q.shift();
+		if (!node) {
+			return false; // because TS
+		}
+
+		if (node.value === needle) {
+			return true;
+		}
+
+		if (node.left) {
+			q.push(node.left);
+		}
+		if (node.right) {
+			q.push(node.right);
+		}
+	};
+
+	return false;
 }
