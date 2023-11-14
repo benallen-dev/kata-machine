6b6f5298c149d43f2660712d617a2c97ca66b043
Ben Allen <ben@benallen.dev>
2023-11-14 19:40:38 +0100

    Implement DFS on BST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,23 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function find(node: BinaryNode<number> | null, needle: number): boolean {
+	if (!node) {
+		return false;
+	}
 
+	if (node.value === needle) {
+		return true;
+	}
+
+	if (node.value < needle) {
+		return find(node.right, needle);
+	}
+	if (node.value > needle) {
+		// Yes this could be just the default case but this is more explicit
+		return find(node.left, needle);
+	}
+
+	return false;
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+	return find(head, needle);	
 }
